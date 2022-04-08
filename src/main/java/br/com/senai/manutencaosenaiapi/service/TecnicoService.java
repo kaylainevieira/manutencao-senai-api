46198905d65d6aae1a67b600a5c7c7f2a9d8d18c
Kaylaine Vieira <kaylainevieira3@gmail.com>
2022-04-07 21:51:33 -0300

    Validação da entidade Tecnico.java usando bean validation

---
+++ src/main/java/br/com/senai/manutencaosenaiapi/service/TecnicoService.java
@@ -0,0 +1,23 @@
+package br.com.senai.manutencaosenaiapi.service;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+import com.google.common.base.Preconditions;
+
+import br.com.senai.manutencaosenaiapi.entity.Tecnico;
+
+@Service
+@Validated
+public class TecnicoService {
+
+	public Tecnico inserir(@Valid @NotNull(message = "O técnico não pode ser nulo.") Tecnico novoTecnico) {
+		
+		
+		Tecnico tecnicoSalvo = novoTecnico;
+		return tecnicoSalvo;
+	}
+}
